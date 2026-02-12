f4fd1e5dab285f9d0720e43b31a3f184df086551
CsermakViktor <vikuszy74@gmail.com>
2026-02-12 18:13:00 +0100

    Missing the controller and the badge files

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $baseBid = $this->faker->numberBetween(10000, 100000);
+        $roundedBid = round($baseBid / 1000) * 100;
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
+            'image_url' => $this->faker->imageUrl(),
+            'starter_bid' => $roundedBid * 1000,
+            'bid_start_date' => now(),
+            'bid_end_date' => now()->addDays(7)
+        ];
+    }
+}
---
+++ database/migrations/2026_02_12_165109_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('products', function (Blueprint $table) {
+
+
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image_url');
+            $table->integer('starter_bid')->default(1000)->round(-3);
+            $table->date('bid_start_date')->default(now());
+            $table->date('bid_end_date')->default(now()->addDays(7));
+            $table->enum('category', ['Electronics', 'Books', 'Clothing', 'House', 'Sports', 'Vehicles', 'Jewelry'])->default('Electronics');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
