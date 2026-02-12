22b987878b6a38ab9d310743a3d751d334f10a1c
vcser <Vcsermak41@gmail.com>
2026-02-12 18:13:00 +0100

    Missing the controller and the badge files

---
+++ app/Models/Bid.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bid extends Model
+{
+    /** @use HasFactory<\Database\Factories\BidFactory> */
+    use HasFactory;
+
+    protected $fillable = ['amount'];
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Bid;
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'description', 'image_url', 'starter_bid', 'bid_end_date'];
+    public function bids()
+    {
+        return $this->hasMany(Bid::class);
+    }
+}
