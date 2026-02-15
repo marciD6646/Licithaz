5c43e2d54c5834758534f9e5023cb3c7155e891f
Marcell <deakvari.marcell@blathy.info>
2026-02-15 17:21:57 +0100

    admin user letrehozas es hasznalata

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,61 +8,41 @@ use App\Http\Requests\UpdateProductRequest;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    /* Display a listing of the resource.*/
     public function index()
     {
         $products = Product::all();
         return view('products.index', ['products' => $products]);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
+    /* Show the form for creating a new resource.*/
     public function create()
     {
         return view('products.create');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
+    /* Store a newly created resource in storage.*/
     public function store(StoreProductRequest $request)
     {
         Product::create($request->validated());
         return redirect()->route('products.index');
     }
-
-    /**
-     * Display the specified resource.
-     */
+    /* Display the specified resource. */
     public function show(Product $product)
     {
         return view('products.show', ['product' => $product]);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
+    /* Show the form for editing the specified resource.*/
     public function edit(Product $product)
     {
         return view('products.edit', ['product' => $product]);
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
+    /* Update the specified resource in storage.*/
     public function update(UpdateProductRequest $request, Product $product)
     {
         $oldProduct = $product->name;
         $product->update($request->all());
         return redirect()->route('products.index')->with('success', "Product $oldProduct updated successfully.");
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
+    /* Remove the specified resource from storage.*/
     public function destroy(Product $product)
     {
         $product->delete();
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,31 @@
     <nav class="bg-gray-800 p-4 text-white gap-2.5 flex flex-row flex-nowrap justify-around">
         <a class="hover:text-gray-300" href="{{ route('welcome') }}">Home</a>
         <a class="hover:text-gray-300" href="{{ route('products.index') }}">View Products</a>
-        <a class="hover:text-gray-300" href="{{ route('products.create') }}">Add New Product</a>
         <a class="hover:text-gray-300" href="">About us</a>
-        <a class="hover:text-gray-300" href="">(admin dashboard)</a>
+        @guest
+        <a class="hover:text-gray-300" href="{{ route('login') }}">Login</a>
+        <a class="hover:text-gray-300" href="{{ route('register') }}">Register</a>
+        
+       
+        @endguest
+        @auth
+        @if (Auth::user()->is_admin)
+        
+            <a class="hover:text-gray-300" href="">Admin Dashboard</a>
+            <a class="hover:text-gray-300" href="">Add New Product</a>
+        
+        
+        @endif
         <a class="hover:text-gray-300" href="">Profile</a>
+        <a class="hover:text-gray-300" href="{{ route('logout') }}"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            Logout
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+        @csrf
+    </form>
+        @endauth
+        
     </nav>
 
     @yield('content')
