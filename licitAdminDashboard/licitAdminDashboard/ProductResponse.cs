cfbea569c678de56d318bca453eb24625df00750
Marcell <deakvari.marcell@blathy.info>
2026-03-25 18:50:16 +0100

    konzolos alap admin dashboard hozza adva + user api controller + bid api controller javitott verzio

---
+++ licitAdminDashboard/licitAdminDashboard/ProductResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace licitAdminDashboard.Models
+{
+    public class ProductResponse
+    {
+        [JsonPropertyName("products")]
+        public List<Product> Products { get; set; }
+    }
+}
